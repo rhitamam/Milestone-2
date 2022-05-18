662b4da2f39571721c033ca6719f179a09d1866b
Traversie Thomas <thomas.traversie@student-cs.fr>
2022-05-18 22:13:32 +0200

    Commentaires

--- src/main/scala/shared/predictions.scala
+++ src/main/scala/shared/predictions.scala
@@ -94,6 +94,7 @@ package object predictions
 
   // Useful functions
 
+  // Calculates the Mean Absolute Error for all the predictions 
   def maeCSC(predictor : (Int,Int) => Double, test : CSCMatrix[Double]) : Double = {
     var s = 0.0
     for ((k,v) <- test.activeIterator) {
@@ -104,10 +105,12 @@ package object predictions
     s/test.activeSize
   }
 
+  // As the indices of our matrix is from 0, we shift the index to take the right user and the upper item (in the matrix)
   def simkNN(u : Int, v : Int, simMatrix : CSCMatrix[Double]) : Double = {
     simMatrix(u-1,v-1)
   }
 
+  // Computes the matrix product bewteen a matrix and its transpose by columns (less memory, but lower that using *)
   def prodMat(mat : CSCMatrix[Double], row : Int, col : Int) : CSCMatrix[Double] = {
     val builder = new CSCMatrix.Builder[Double](rows=row, cols=row)
     for (v <- 0 to row-1){ 
@@ -119,6 +122,7 @@ package object predictions
     builder.result()
   }
 
+  // Computes the matrix product bewteen a matrix and its transpose element per element (less memory, but lower that using *)
   def prodMat2(mat : CSCMatrix[Double], row : Int, col : Int) : CSCMatrix[Double] = {
     val builder = new CSCMatrix.Builder[Double](rows=row, cols=row)
     for (u <- 0 to row -1){
@@ -129,6 +133,7 @@ package object predictions
     builder.result()
   }
 
+  // Reduction of a matrix using a DenseVector
   def reduction(mat : CSCMatrix[Double],users : Int) : DenseVector[Double] = {
     var vect = DenseVector.zeros[Double](users)	
     for ((k,v) <- mat.activeIterator) {
@@ -139,6 +144,7 @@ package object predictions
     vect
   }
 
+  // Basic vector of 1 to help us compute in the reduction
   def vectorOnes(n : Int) : CSCMatrix[Double] = {
     val builder = new CSCMatrix.Builder[Double](rows=n, cols=1)
     for (k <- 0 to n-1) {
@@ -149,10 +155,12 @@ package object predictions
 
   // Part 3: Optimizing with Breeze, a Linear Algebra Library
 
+  // Compute the Average Rating of all Users (global Average)
   def avgRating(ratings : CSCMatrix[Double]) : Double = {
     sum(ratings)/ratings.activeSize
   }
 
+  // Calcultates the average Rating for all users with a CSC Matrix (will output a N*1 Matrix with N the number of users)
   def avgRatingUserMatrix(ratings : CSCMatrix[Double], users : Int, movies : Int) : CSCMatrix[Double] = {
     val counts = Array.fill(users)(0)
     val builder = new CSCMatrix.Builder[Double](rows=users, cols=1)
@@ -176,6 +184,7 @@ package object predictions
     (r_u_i - r_u)/scale(r_u_i, r_u)
   }
 
+  // Calculates the normalized Matrix
   def normalizedDevMatrix(ratings : CSCMatrix[Double], avgMatrix : CSCMatrix[Double], users : Int, movies : Int) : CSCMatrix[Double] = {
     val builder = new CSCMatrix.Builder[Double](rows=users, cols=movies)
     for ((k,v) <- ratings.activeIterator) {
@@ -186,6 +195,7 @@ package object predictions
     builder.result()
   }
 
+  // We preprocess the matrix as last milestone but with CSCMatrix
   def processedMatrix(normalizedMatrix : CSCMatrix[Double], users : Int, movies : Int) : CSCMatrix[Double] = {
     var sumSquareMatrix = (normalizedMatrix *:* normalizedMatrix) * vectorOnes(movies) //if we use the function vectorOnes
     //var sumSquareMatrix = reduction(normalizedMatrix *:* normalizedMatrix,users) //if we use the function reduction
@@ -199,6 +209,7 @@ package object predictions
     builder.result()
   }
 
+  // Calculates kNN similarity with CSCMatrix
   def simOpt(k : Int, preProcessedMatrix : CSCMatrix[Double], users : Int, movies : Int) : CSCMatrix[Double] = {
     var simMat = prodMat(preProcessedMatrix,users,movies)
     val builder = new CSCMatrix.Builder[Double](rows=users, cols=users)
@@ -210,6 +221,7 @@ package object predictions
     builder.result()
   }
 
+  // Calculates the weighted sum deviation for a user
   def kNNRating(user : Int, item : Int, normalizedMatrix : CSCMatrix[Double], simMatrix : CSCMatrix[Double],users : Int, ratings : CSCMatrix[Double]) : Double = {
     var num = 0.0
     var denom = 0.0
@@ -222,6 +234,7 @@ package object predictions
     if (denom != 0) num/denom else 0.0
   }
 
+  // Calculates the exact kNN predictor with CSCMatrix
   def predictionKNN(k : Int, ratings : CSCMatrix[Double], users : Int, movies : Int) : ((Int,Int) => Double) = {
     val avg = avgRating(ratings)
     val avgMatrix = avgRatingUserMatrix(ratings,users,movies)
@@ -238,12 +251,17 @@ package object predictions
 
   // Part 4: Parallel k-NN Computations with Replicated Ratings
 
+  // For a user, finds the top k most similar users
   def topk(u : Int, br : org.apache.spark.broadcast.Broadcast[CSCMatrix[Double]], k : Int, movies : Int) : (Int,Seq[(Int,Double)]) = {
     val r = br.value
+    // Broadcast variable retrieval (matrix)
     val sim_u = r * r(u,0 to movies-1).t
+    // Computes the similarity between user u and every other user
     (u,argtopk(sim_u,k+1).filter(_ != u).map(x => (x, sim_u(x))))
+    // Output of the form : (user u, Seq(user v, similarity between u and v))
   }
 
+  // Computes directly the similarity between u and v
   def simkNNparallelized(u : Int, v : Int, k : Int, preProcessedMatrix : CSCMatrix[Double], movies : Int) : Double = {
     val sim_u = preProcessedMatrix * preProcessedMatrix(u-1,0 to movies-1).t
     if (argtopk(sim_u,k+1).filter(_ != u-1) contains v-1) {
@@ -251,6 +269,7 @@ package object predictions
     } else 0
   }
 
+  // Computes the parallel kNN predictor using multiple workers
   def parallelizedKNN(ratings : CSCMatrix[Double], k: Int, sc : SparkContext, users : Int, movies : Int): ((Int,Int) => Double) = {
     val avg = avgRating(ratings)
     val avgMatrix = avgRatingUserMatrix(ratings,users,movies)
@@ -258,13 +277,15 @@ package object predictions
     val preProcessedMatrix = processedMatrix(normalizedMatrix,users,movies)
     val br = sc.broadcast(preProcessedMatrix)
     val topku = sc.parallelize(0 to users-1).map(u=> topk(u,br,k,movies)).collect()
+    // Takes top k neerest neighbours for each user
     val builder = new CSCMatrix.Builder[Double](rows= users, cols= users)
+    // Matrix that will contain similarity for each user-item pair
     for (x <- topku) {
       for (y <- x._2) {
         builder.add(x._1,y._1,y._2)
       }
     }
-    val mat = builder.result()
+    val mat = builder.result() // Similarity matrix
     var r_u : Double = 0.0
     ((u,i) => {
     var r_i = kNNRating(u-1,i-1,normalizedMatrix,mat,users,ratings)
@@ -275,6 +296,7 @@ package object predictions
 
   // Part 5: Distributed Approximate k-NN
 
+  // Creates a matrix for a given partition of users
   def createPartMat(list_user : Set[Int], br : org.apache.spark.broadcast.Broadcast[CSCMatrix[Double]], users : Int, movies : Int) : CSCMatrix[Double] = {
     val preProcessedMatrix = br.value
     val builder = new CSCMatrix.Builder[Double](rows = users, cols = movies)
@@ -282,12 +304,14 @@ package object predictions
     builder.result()
   }
 
+  // Calculates the similarity for each partition
   def simPartition(k : Int, list_user : Set[Int], users : Int,movies : Int, br : org.apache.spark.broadcast.Broadcast[CSCMatrix[Double]]) : Seq[(Int,Seq[(Int,Double)])] = {
       val partMat = createPartMat(list_user, br,users,movies)
       var simMat = prodMat(partMat,users,movies)
       (list_user.toSeq).map(u => (u,argtopk(simMat(u,0 to users-1).t,k+1).filter(_ != u).map(v=>(v,simMat(u,v)))))
   }
 
+  // Concatenates all similarities of all partitions, taking the k nearest neighbours
   def simApprox(preProcessedMatrix : CSCMatrix[Double], k: Int, sc : SparkContext, partitions : Seq[Set[Int]], users : Int, movies : Int): CSCMatrix[Double] = {
     val br = sc.broadcast(preProcessedMatrix)
     var partitionRDD = sc.parallelize(partitions).flatMap(p=> simPartition(k, p, users,movies,br)).reduceByKey{case (x,y) => x.union(y).distinct}.collect()
@@ -301,6 +325,7 @@ package object predictions
     simMat
   }
 
+  // Computes the approximate kNN predictor using the final similarity matrix
   def approximateKNN(ratings : CSCMatrix[Double], k: Int, sc : SparkContext, partitions : Seq[Set[Int]], users :Int, movies : Int): ((Int,Int) => Double) = {
     val avg = avgRating(ratings)
     val avgMatrix = avgRatingUserMatrix(ratings,users,movies)
